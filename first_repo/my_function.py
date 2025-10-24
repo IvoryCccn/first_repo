9a346c0e53a268b5501e1c136baf5a3c26af389f
我是不是有点胖了 <cn_553512@163.com>
2025-10-25 00:53:04 +0100

    Modity the document to add a new function

--- first_repo/my_function.py
+++ first_repo/my_function.py
@@ -1,2 +1,5 @@
 def my_function():
     print("Hello from my_function!")
+
+def self_introduction():
+    print("Hello, my name is Ning Chen, and it's better to call my Ivory. I'm studying in Mphil in Economics and Data Science, coming from Hughes Hall. Nice to meet you")
